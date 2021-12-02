9d26aa19a15af69c6f01e42999b8f3ec8f2ca1af
Arthur Reading <artrea@gmail.com>
2021-12-01 22:24:48 -0800

    Ship problem

--- src/clojuretest/core.clj
+++ src/clojuretest/core.clj
@@ -10,17 +10,52 @@
 (defn moving-sum [n coll]
   (map sum (partition n 1 coll)))
 
+(defn parse-int [s] (Integer/parseInt s))
+
 (defn problem1 []
-  (let [integers (vec (->>
-                        (slurp "inputs/1.txt")
-                        (clojure.string/split-lines)
-                        (map #(Integer/parseInt %))))]
+  (let [integers (->>
+                   (slurp "inputs/1.txt")
+                   (clojure.string/split-lines)
+                   (map parse-int))]
     (println (count-increases integers))
     (println (count-increases (moving-sum 3 integers)))))
 
-(let [functions [problem1]]
+(defn parse-movement [movement-string]
+  (let [[dir-string distance-string] (clojure.string/split movement-string #" ")
+        distance (parse-int distance-string)]
+    (case dir-string
+      "forward" [0 distance]
+      "backward" [0 (- distance)]
+      "down" [distance 0]
+      "up" [(- distance) 0])))
+
+(defn add-movements1 [m1 m2]
+  [(+ (first m1) (first m2))
+   (+ (second m1) (second m2))])
+
+(defstruct ship :h :x :aim)
+
+(defn add-movements2 [s m]
+  (struct ship (+ (get s :h) (* (second m) (get s :aim)))
+          (+ (get s :x) (second m))
+          (+ (get s :aim) (first m))))
+
+(defn problem2 []
+  (let [movements (->>
+                    (slurp "inputs/2.txt")
+                    (clojure.string/split-lines)
+                    (map parse-movement))
+        finalpos1 (reduce add-movements1 movements)
+        finalpos2 (reduce add-movements2 (struct ship 0 0 0) movements)]
+    (println (reduce * finalpos1))
+    (println (* (get finalpos2 :x) (get finalpos2 :h)))))
+
+
+(let [functions [problem1
+                 problem2]]
   (defn -main
     "Main entry point"
     []
-    (println "Enter problem to solve: ")
+    (print "Enter problem to solve: ")
+    (flush)
     ((get functions (- (Integer/parseInt (read-line)) 1)))))
